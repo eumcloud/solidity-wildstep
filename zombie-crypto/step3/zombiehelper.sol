447859e232b6bd01f9fe8392e228b06579bcb1fe
eumcloud <livelch012@naver.com>
2022-04-15 16:48:41 +0900

    Lesson3

--- zombie-crypto/step3/zombiehelper.sol
+++ zombie-crypto/step3/zombiehelper.sol
@@ -4,9 +4,15 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+    uint levelUpFee = 0.001 ether;
     modifier aboveLevel(uint _level, uint _zombieId){
         require(zombies[_zombieId].level >= _level);
         _;
+    }
+
+    function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
     }
 
     function changeName(uint _zombieId, string _newName) external abovelevel(2, _zombieId){
